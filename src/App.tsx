57816cad2fbe0cd0a31876be8e07b84d2f5d5ebc
CristnaAT <alumno.668286@ies-azarquiel.es>
2023-03-02 17:47:29 +0100

    Añadimos config proxy y cambio backend=/api/hora

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const backend: string='/api/hora'
+const refresco: number=3000
+
 function App() {
+  // Creo una variable de estado
+  const [hora, setHora] = useState(null);
+
+  //Indicamos a React que active el intervalo después de renderizar
+  //pero lo desactive al desmontar
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch(backend)
+        .then(response => response.json())
+        .then(json => setHora(json.Hora));
+    }, refresco);
+    return () => {
+        clearInterval(interval);
+      };
+  }, []);
+  //intervalo()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {hora}
         </p>
         <a
           className="App-link"
